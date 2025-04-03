55aff822d59b04da483c6c9f7ac8ec80d2cc17bc
jhums04 <jbonganciso04@gmail.com>
2025-04-03 23:13:07 +0800

    Remvoe unused import
    - added due to sonar testing for pass and fail scenario

--- src/test/java/com/example/demo/controller/HelloControllerTest.java
+++ src/test/java/com/example/demo/controller/HelloControllerTest.java
@@ -3,8 +3,6 @@ package com.example.demo.controller;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
